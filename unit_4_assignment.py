f429b43b5881e77f0e54eb9d927e8bdce2aaef18
Kwame <bkwamesakyi@gmail.com>
2019-10-15 10:17:10 -0400

    I added my unit 4 flowchart. I started my unit 4 assignment. I finished my unit 4 assignment.

--- unit_4_assignment.py
+++ unit_4_assignment.py
@@ -8,13 +8,32 @@ def address():
 def age():
     return int(input("How old are you?"))
 
-def price(location, price_1):
-    if location == "M":
-        return price_1 == 60
-    else:
-        return price_1 == 70
 
-def discounts(location, age_v, price_1):
+def discounts(location, age_v):
+    if age_v < 5:
+        return 0
+    elif age_v >= 65:
+        if location == "M" or "E":
+            return 30
+        else:
+            return 60
+    elif location == "H":
+        if age_v < 14:
+            return 28.70
+        else:
+            return 35
+    elif location == "PG":
+        if age_v >= 65:
+            return 32.36
+        else:
+            return 35
+    else:
+        if location == "E":
+            return 70
+        if location == "E" and age_v >= 65:
+            return 35
+        if location == "M" or "H" or "E" or "PG" and 5 <= age_v < 65:
+            return 70
 
 
 
@@ -22,7 +41,10 @@ def main():
     print("Welcome to the Adventure Park Ticket Booth!")
     location = address()
     age_v = age()
-    price_1 = price(location, price_1)
     if age_v <= 0:
-       return int(input("The age entered is invalid. Please enter valid age."))
-    discounts(location, age_v, price_1)
+        return int(input("The age entered is invalid. Please enter valid age."))
+    discounts(location, age_v)
+    print("Your Ticket price is", discounts(location, age_v))
+
+
+main()
